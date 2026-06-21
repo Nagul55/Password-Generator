a270f9ed5f88111ada9fd083b73bd7111346d767
Nagul55 <nagulworkspace@gmail.com>
2026-06-21 19:58:15 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let firstEl = document.getElementById("first-password")
+let secondEl = document.getElementById("second-password")
+let isgenerated = false
+
+function generate() {
+    firstEl.textContent = ""
+    secondEl.textContent = ""
+
+    for(let i = 0; i < 16; i++) {
+        firstEl.textContent += characters[Math.floor(Math.random() * characters.length)]
+        secondEl.textContent += characters[Math.floor(Math.random() * characters.length)]
+    }
+}
+
